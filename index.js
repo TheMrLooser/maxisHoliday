1498d303bc466d6dc9f44e418ed0039f0ee53213
Ankush Kumar <ankushsoni7171@gmail.com>
2022-09-19 23:00:36 +0530

    cors added

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const clientRouter = require('./Router/client');
 const bodyParser = require('body-parser');
 const employeeRouter = require('./Router/employee');
 dotenv.config({path:'config/config.env'})
+const cors = require('cors')
+
+
 
 const app = express()
 app.use(express.json())
 app.use("/client",clientRouter);
 app.use("/employee",employeeRouter);
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods: ['GET', 'PUT', 'POST','DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'], 
+    credentials: true, 
+    maxAge: 600, 
+    exposedHeaders: ['*', 'Authorization' ] 
+}))
 
 connectDB()
 app.listen(process.env.PORT,()=>{
