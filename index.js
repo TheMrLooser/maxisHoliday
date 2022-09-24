d0a417c5389217f87dd87ab2dbdb59b78e1ac0f4
Ankush Kumar <ankushsoni7171@gmail.com>
2022-09-24 15:32:56 +0530

    origin change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:['http://localhost:3000','https://maxisholiday.netlify.app'],
     methods: ['GET', 'PUT', 'POST','DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'], 
     credentials: true, 
